95b0b0d44ef80d432d93d79dbc559e031dd63761
Orie Steele <orie@ohs.io>
2019-08-04 13:40:35 -0500

    test(demo): test github docs deploy

--- docs/demo/service-worker.js
+++ docs/demo/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b6cec5da2d2f936329d2d47536132682.js"
+  "/lds-ecdsa-secp256k1-2019.js/demo/precache-manifest.6524f4ae4815be9716a2a2cd593876ff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lds-ecdsa-secp256k1-2019.js/demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
