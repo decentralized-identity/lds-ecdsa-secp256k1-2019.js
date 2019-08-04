0ac69fa7b99eb40e4413558af9632648de9352e2
Orie Steele <orie@ohs.io>
2019-08-04 13:13:25 -0500

    chore(package): fix prettier for lerna

--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,5 @@
 # Documentation
 
 #### [Latest Spec](https://identity.foundation/lds-ecdsa-secp256k1-2019.js/)
+
+#### [ES256K JWS](./es256k-jws-ts)
---
+++ scripts/copy-docs.sh
@@ -0,0 +1,2 @@
+cp -r ./packages/es256k-jws-ts/docs/ ./docs/es256k-jws-ts/
+cp -r ./packages/lds-ecdsa-secp256k1-2019/docs/ ./docs/lds-ecdsa-secp256k1-2019/
