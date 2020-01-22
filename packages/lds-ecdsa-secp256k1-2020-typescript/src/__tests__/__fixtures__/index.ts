6874eaa2a2a025f16093b394a2fee093eda555aa
Orie Steele <orie@ohs.io>
2020-01-22 12:46:01 -0600

    chore: wip refactor for lds-jose2020

---
+++ packages/lds-ecdsa-secp256k1-2020-typescript/src/__tests__/__fixtures__/index.ts
@@ -0,0 +1,7 @@
+import didDoc from './didDoc.json';
+import didDocJwks from './didDocJwks.json';
+
+export default {
+  didDoc,
+  didDocJwks,
+};
---
+++ packages/lds-ecdsa-secp256k1-2020-typescript/src/lib/index.d.ts
@@ -0,0 +1,4 @@
+declare module 'jsonld';
+
+// dev deps
+declare module 'jsonld-signatures';
